7e93353e96b26ad95c3b58d3c1ead46cc65c6d4b
Matthias Gst <matthias.gaste@epitech.eu>
2023-12-11 14:53:05 +0100

    Archi(Web): init react web application with basic page (#17)
    * arch(Web): add the react web application
    
    * fix(Web): delete useless file
    
    * archi(Web): add folders
    
    * feat(Web): add basic page Hellow world with logo and style
    
    * refactor(Web): delete useless comments
    
    * feat(Web): link basic page to App.js
    
    * fix(Web): fix error in CSS
    
    ---------
    
    Co-authored-by: quentin <Quentin.brejoin@gmail.com>

---
+++ area_front_web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorldPage from './pages/HelloWorldPage';
+
+function App() {
+  return (
+    <div>
+      <HelloWorldPage />
+    </div>
+  );
+}
+
+export default App;
