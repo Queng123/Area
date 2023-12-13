1fbd5f440d7edf87b0bb27746ec99390ce6ce384
Lylian Hay <91667138+VyOk9@users.noreply.github.com>
2023-12-13 10:32:18 +0100

    feat(Api Routes): Api routes documentation with swagger ui (#21)
    * feat(package): add the swagger ui
    
    * feat(main): add the swagger ui in main
    
    * refactor(modules): add the all controller for endpoints basic
    
    * feat(action): create the controller for swagger ui
    
    * feat(auth): create the controller for swagger ui
    
    * feat(areas): create the controller for swagger ui
    
    * feat(reactions): create the controller for swagger ui
    
    * feat(services): create the controller for swagger ui
    
    * feat(users): create the controller for swagger ui
    
    * refactor(swagger-default)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Area-API')
+    .setDescription('The Area-API is a RESTful API that allows you to manage the Epitech ARea project.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
