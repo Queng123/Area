4cd407359a88b75cbe0c21d1360384ba67d76ba5
Lylian Hay <91667138+VyOk9@users.noreply.github.com>
2023-12-18 09:03:16 +0100

    Ci(workflow): Create a step for in-flutter environment variables (#52)
    * ci(workflow): add the secrets env test 1
    
    * feat(pubspec): add the fake .env for testing the ci/ci
    
    * feat(pubspec): add the dotenv
    
    * feat(main): add the dotenv in flutter

--- area_front_app/lib/main.dart
+++ area_front_app/lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:area_front_app/pages/auth/login_page.dart';
 import 'package:area_front_app/pages/auth/register_page.dart';
 import 'package:area_front_app/pages/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  if (dotenv.env['BASE_URL'] == null) {
+    throw Exception('BASE_URL is not defined');
+  }
   runApp(const MyApp());
 }
 
