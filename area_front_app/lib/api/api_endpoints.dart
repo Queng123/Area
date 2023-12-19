fdb26c46cc0b517f8bd492f20d38c12687ae5e76
Lylian Hay <91667138+VyOk9@users.noreply.github.com>
2023-12-19 11:06:31 +0100

    refactor(api_endpoints): new route for register/email (#72)

--- area_front_app/lib/api/api_endpoints.dart
+++ area_front_app/lib/api/api_endpoints.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class ApiRoutes {
   static String baseUrl = dotenv.env['BASE_URL'] ?? '';
 
-  static const String signUpEmail = 'user/signup/email';
+  static const String signUpEmail = 'user/register/email';
   static const String loginEmail = 'user/login/email';
   static const String loginGoogle = 'user/login/google';
   static const String logout = 'user/logout';
