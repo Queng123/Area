f0da568d35b81b5f60f661fba4c3c13a83d02a7a
Matthias Gst <matthias.gaste@epitech.eu>
2024-01-02 23:24:33 +0100

    feat(Web): Redesign Login Page (#140)
    * fix(Web): fix url Web, delete '#' from url
    
    * refactor(Web): redesign Register Page
    
    * feat(Web): improve css to have design responsible
    
    * refactor(Web): delete useless Google service
    
    * refactor(Web): replace useless routes
    
    * feat(Web): style adaptable to mobile, ipad, compute
    
    * feat(Web): add responsible text on Register Page
    
    * feat(Web): add redirection to login page in Register Page
    
    * feat(Web): add inputs and buttons in form area
    
    * feat(Web): add input verifications with error messages
    
    * feat(Web): increase the size max of the square
    
    * feat(Web): In Register Page, add error message if the password is too small
    
    * feat(Web): add registration with error message if fail for Register Page
    
    * feat(Web): add success message and handle differents error
    
    * feat(Web): load env from .env file
    
    * feat(Web): use env to request API
    
    * feat(Web): adapt error/success message for mobile
    
    * fix(Web): delete useless deps
    
    * feat(Web): redesign Login Page

--- area_front_web/src/pages/Login.tsx
+++ area_front_web/src/pages/Login.tsx
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
 
+import './styles/Login.css';
+import logo from '../assets/logo.png';
+import googleLogo from '../assets/googleLogo.png';
+
 export function Login() {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='body'>
+            <div className='square'>
+                <img src={logo} alt="Logo" className="logo" />
+                <form className="form">
+                    <input
+                        type="email"
+                        placeholder="Address Email"
+                        name="email"
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                    />
+                    <span className="forgot-password">Forgot Password?</span>
+
+                    <button className="login-button" type="submit">Login</button>
+                </form>
+                <div className="separator-text">Or continue with</div>
+                <img src={googleLogo} alt="Google Logo" className="google-logo" />
+                <div className="text-already-account">
+                    Not a member?
+                    <span className="blue-text-link" >Register here</span>
+                </div>
+            </div>
         </div>
     );
 };
-
