56be2ad6d31887736164d34e7b0e5b0ff0f16094
Matthias Gst <matthias.gaste@epitech.eu>
2024-01-06 11:22:46 +0100

    feat(Web): add logout button (#182)

--- area_front_web/src/pages/Home.tsx
+++ area_front_web/src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import './styles/Home.css';
 
 export function Home() {
+    const navigate = useNavigate();
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+    const handleClick = async () => {
+        const response = await axios.get(`${BASE_URL}/user/logout`);
+
+        console.log('"Server response:', response.data);
+        console.log(`${BASE_URL}/user/logout`);
+        navigate('/login');
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <button className='button-circle' onClick={handleClick}>Log Out</button>
+        </div>
     )
 }
