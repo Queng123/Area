3fb72d113f7cde6bc0c82658a2e1300a13e658df
Matthias Gst <matthias.gaste@epitech.eu>
2024-01-06 14:02:35 +0100

    feat(Web): add update password field (#186)

--- area_front_web/src/pages/Home.tsx
+++ area_front_web/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -7,6 +7,12 @@ import './styles/Home.css';
 export function Home() {
     const navigate = useNavigate();
     const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+    const [password, setPassword] = useState<string>('');
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
 
     const handleClick = async () => {
         const response = await axios.get(`${BASE_URL}/user/logout`);
@@ -23,10 +29,27 @@ export function Home() {
         navigate('/login');
     }
 
+    const UpdatePassword = async () => {
+        const requestData = {
+            password: password,
+        };
+        console.log(requestData);
+        const response = await axios.post(`${BASE_URL}/user/update-password`, requestData);
+        console.log('Server response:', response.data);
+        setPassword('');
+    }
     return (
         <div>
             <button className='button-circle' onClick={handleClick}>Log Out</button>
             <button className='button-circle' onClick={DeleteAccount}>Delete account</button>
+            <button className='button-circle' onClick={UpdatePassword}>Update Password</button>
+            <label>New Password :</label>
+            <input
+                type='password'
+                placeholder='enter new password'
+                value={password}
+                onChange={handlePasswordChange}
+            />
         </div>
     )
 }
