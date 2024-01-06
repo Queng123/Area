4fe83e61e4ffcc56ae2ee07316db2603a1dd174a
Matthias Gst <matthias.gaste@epitech.eu>
2024-01-06 11:37:02 +0100

    feat(Web): add delete account button (#183)

--- area_front_web/src/pages/Home.tsx
+++ area_front_web/src/pages/Home.tsx
@@ -11,14 +11,22 @@ export function Home() {
     const handleClick = async () => {
         const response = await axios.get(`${BASE_URL}/user/logout`);
 
-        console.log('"Server response:', response.data);
+        console.log('Server response:', response.data);
         console.log(`${BASE_URL}/user/logout`);
+        navigate('/login');
+    }
+
+    const DeleteAccount = async () => {
+        const response = await axios.delete(`${BASE_URL}/user`);
+
+        console.log('Server response:', response.data);
         navigate('/login');
     }
 
     return (
         <div>
             <button className='button-circle' onClick={handleClick}>Log Out</button>
+            <button className='button-circle' onClick={DeleteAccount}>Delete account</button>
         </div>
     )
 }
